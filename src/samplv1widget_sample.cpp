0078eb224ebecbf6e5b022446d233c5fa4ac9ca4
rncbc <rncbc@b8ad5884-86ea-42fc-91ed-2b8b5c17ad06>
2012-12-10 16:58:51 +0000

    - Preliminary sample widget sub-loop support.
    git-svn-id: http://svn.code.sf.net/p/samplv1/code_attic/trunk@76 b8ad5884-86ea-42fc-91ed-2b8b5c17ad06

--- src/samplv1widget_sample.cpp
+++ src/samplv1widget_sample.cpp
@@ -42,9 +42,9 @@ samplv1widget_sample::samplv1widget_sample (
 	QWidget *pParent, Qt::WindowFlags wflags )
 	: QFrame(pParent, wflags), m_pSample(0), m_iChannels(0), m_ppPolyg(0)
 {
-	setMouseTracking(true);
-	setFocusPolicy(Qt::ClickFocus);
-	setMinimumSize(QSize(580, 80));
+	QFrame::setMouseTracking(true);
+	QFrame::setFocusPolicy(Qt::ClickFocus);
+	QFrame::setMinimumSize(QSize(580, 80));
 
 	QFrame::setFrameShape(QFrame::Panel);
 	QFrame::setFrameShadow(QFrame::Sunken);
@@ -197,7 +197,7 @@ void samplv1widget_sample::mousePressEvent ( QMouseEvent *pMouseEvent )
 				m_dragState = DragStart;
 				m_posDrag = pMouseEvent->pos();
 			} else {
-				const int w = QWidget::width();
+				const int w = QFrame::width();
 				const uint32_t nframes = m_pSample->length();
 				if (nframes > 0) {
 					m_iDragStartX = (m_iLoopStart * w) / nframes;
@@ -219,7 +219,7 @@ void samplv1widget_sample::mouseMoveEvent ( QMouseEvent *pMouseEvent )
 	switch (m_dragState) {
 	case DragNone: {
 		if (m_bLoop && m_pSample) {
-			const int w = QWidget::width();
+			const int w = QFrame::width();
 			const uint32_t nframes = m_pSample->length();
 			if (nframes > 0) {
 				const int x1 = (m_iLoopStart * w) / nframes;
@@ -288,7 +288,7 @@ void samplv1widget_sample::mouseReleaseEvent ( QMouseEvent *pMouseEvent )
 
 	switch (m_dragState) {
 	case DragSelect: {
-		const int w = QWidget::width();
+		const int w = QFrame::width();
 		if (m_pSample && w > 0) {
 			const uint32_t nframes = m_pSample->length();
 			m_iLoopStart = (m_iDragStartX * nframes) / w;
@@ -299,7 +299,7 @@ void samplv1widget_sample::mouseReleaseEvent ( QMouseEvent *pMouseEvent )
 		break;
 	}
 	case DragLoopStart: {
-		const int w = QWidget::width();
+		const int w = QFrame::width();
 		if (m_pSample && w > 0) {
 			const uint32_t nframes = m_pSample->length();
 			m_iLoopStart = (m_iDragStartX * nframes) / w;
@@ -309,7 +309,7 @@ void samplv1widget_sample::mouseReleaseEvent ( QMouseEvent *pMouseEvent )
 		break;
 	}
 	case DragLoopEnd: {
-		const int w = QWidget::width();
+		const int w = QFrame::width();
 		if (m_pSample && w > 0) {
 			const uint32_t nframes = m_pSample->length();
 			m_iLoopEnd = (m_iDragEndX * nframes) / w;
@@ -365,7 +365,7 @@ void samplv1widget_sample::paintEvent ( QPaintEvent *pPaintEvent )
 {
 	QPainter painter(this);
 
-	const QRect& rect = QWidget::rect();
+	const QRect& rect = QFrame::rect();
 	const int h = rect.height();
 	const int w = rect.width();
 
