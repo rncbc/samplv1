67de7152470002ab4d82bdc874f5caa6b88b3339
rncbc <rncbc@b8ad5884-86ea-42fc-91ed-2b8b5c17ad06>
2014-01-22 18:22:10 +0000

    - Reverb added as new final effects stage (based on FreeVerb).   (HIGHLY EXPERIMENTAL)
    git-svn-id: http://svn.code.sf.net/p/samplv1/code_attic/trunk@215 b8ad5884-86ea-42fc-91ed-2b8b5c17ad06

--- src/samplv1.cpp
+++ src/samplv1.cpp
@@ -445,6 +445,7 @@ struct samplv1_dyn
 struct samplv1_rev
 {
 	float *wet;
+	float *feedb;
 	float *room;
 	float *damp;
 	float *width;
@@ -1064,6 +1065,7 @@ void samplv1_impl::setParamPort ( samplv1::ParamIndex index, float *pfParam )
 	case samplv1::DYN1_COMPRESS:  m_dyn.compress     = pfParam; break;
 	case samplv1::DYN1_LIMITER:   m_dyn.limiter      = pfParam; break;
 	case samplv1::REV1_WET:       m_rev.wet          = pfParam; break;
+	case samplv1::REV1_FEEDB:     m_rev.feedb        = pfParam; break;
 	case samplv1::REV1_ROOM:      m_rev.room         = pfParam; break;
 	case samplv1::REV1_DAMP:      m_rev.damp         = pfParam; break;
 	case samplv1::REV1_WIDTH:     m_rev.width        = pfParam; break;
@@ -1142,6 +1144,7 @@ float *samplv1_impl::paramPort ( samplv1::ParamIndex index )
 	case samplv1::DYN1_COMPRESS:  pfParam = m_dyn.compress;     break;
 	case samplv1::DYN1_LIMITER:   pfParam = m_dyn.limiter;      break;
 	case samplv1::REV1_WET:       pfParam = m_rev.wet;          break;
+	case samplv1::REV1_FEEDB:     pfParam = m_rev.feedb;        break;
 	case samplv1::REV1_ROOM:      pfParam = m_rev.room;         break;
 	case samplv1::REV1_DAMP:      pfParam = m_rev.damp;         break;
 	case samplv1::REV1_WIDTH:     pfParam = m_rev.width;        break;
@@ -1618,7 +1621,7 @@ void samplv1_impl::process ( float **ins, float **outs, uint32_t nframes )
 		// reverb
 		if (k > 0) {
 			m_reverb.process(outs[k - 1], outs[k], nframes, *m_rev.wet,
-				*m_rev.room, *m_rev.damp, *m_rev.width);
+				*m_rev.feedb, *m_rev.room, *m_rev.damp, *m_rev.width);
 		}
 	}
 
