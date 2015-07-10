011c3d012a1fd8c3b29f58914fcadc55094ada5a
rncbc <rncbc@gamma.rncbc.lan>
2015-07-10 19:06:13 +0100

    - MIDI Controllers Hook/sync option added.

--- src/samplv1_controls.cpp
+++ src/samplv1_controls.cpp
@@ -567,14 +567,13 @@ void samplv1_controls::process_event ( const Event& event )
 
 	m_sched_in.schedule_key(key);
 
-	Data data;
-	int iIndex = get_control(key, data);
-	if (iIndex < 0 && key.channel() > 0) {
+	Data& data = m_map[key];
+	if (data.index < 0 && key.channel() > 0) {
 		key.status = key.type(); // channel=0 (Auto)
-		iIndex = get_control(key, data);
+		data = m_map[key];
 	}
 
-	if (iIndex < 0)
+	if (data.index < 0)
 		return;
 
 	// process controller event...
@@ -593,8 +592,27 @@ void samplv1_controls::process_event ( const Event& event )
 	if (data.flags & Logarithmic)
 		fValue *= (fValue * fValue);
 
-	const samplv1::ParamIndex index = samplv1::ParamIndex(data.index);
-	m_sched_out.schedule_event(index, samplv1_param::paramValue(index, fValue));
+	const samplv1::ParamIndex index
+		= samplv1::ParamIndex(data.index);
+
+	// normalize value...
+	fValue = samplv1_param::paramValue(index, fValue);
+
+	// catch-up testing begin...
+	bool bSync = (data.flags & Hook);
+
+	if (!bSync) {
+		const float v0 = data.val;
+		const float v1 = m_sched_in.instance()->paramValue(index);
+		if ((fValue > v0 && v1 >= v0 && fValue >= v1) ||
+			(fValue < v0 && v0 >= v1 && v1 >= fValue))
+			bSync = true;
+	}
+
+	if (bSync)
+		m_sched_out.schedule_event(index, fValue);
+
+	data.val = fValue;
 }
 
 
--- src/samplv1widget_control.cpp
+++ src/samplv1widget_control.cpp
@@ -92,6 +92,9 @@ samplv1widget_control::samplv1widget_control (
 	QObject::connect(m_ui.ControlInvertCheckBox,
 		SIGNAL(toggled(bool)),
 		SLOT(changed()));
+	QObject::connect(m_ui.ControlHookCheckBox,
+		SIGNAL(toggled(bool)),
+		SLOT(changed()));
 	QObject::connect(m_ui.DialogButtonBox,
 		SIGNAL(clicked(QAbstractButton *)),
 		SLOT(clicked(QAbstractButton *)));
@@ -169,6 +172,8 @@ void samplv1widget_control::setControls (
 		flags & samplv1_controls::Logarithmic);
 	m_ui.ControlInvertCheckBox->setChecked(
 		flags & samplv1_controls::Invert);
+	m_ui.ControlHookCheckBox->setChecked(
+		flags & samplv1_controls::Hook);
 
 	--m_iDirtySetup;
 
@@ -329,6 +334,8 @@ void samplv1widget_control::accept (void)
 		flags |= samplv1_controls::Logarithmic;
 	if (m_ui.ControlInvertCheckBox->isChecked())
 		flags |= samplv1_controls::Invert;
+	if (m_ui.ControlHookCheckBox->isChecked())
+		flags |= samplv1_controls::Hook;
 
 	// Map the damn controller....
 	samplv1_controls::Data data;
