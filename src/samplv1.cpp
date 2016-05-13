3bf325c8db48dd3ba2cbdf5fb22e445df64e658f
rncbc <rncbc@gamma.rncbc.lan>
2016-05-13 18:30:35 +0100

    - Parameter value pseudo-smoothing fix.

--- src/samplv1.cpp
+++ src/samplv1.cpp
@@ -147,36 +147,38 @@ class samplv1_port
 {
 public:
 
-	samplv1_port() : m_port(NULL), m_value(0.0f) {}
+	samplv1_port() : m_port(NULL), m_value(0.0f), m_cache(false) {}
 
 	void set_port(float *port)
 		{ m_port = port; }
 	float *port() const
 		{ return m_port; }
 
-	void set_value(float value, bool cache = false)
-		{ m_value = value; if (!cache) update(); }
+	virtual void set_value(float value, bool cache)
+		{ m_value = value; m_cache = cache; if (!m_cache) update(); }
 
 	virtual float value(uint32_t /*nstep*/ = 1)
-	{
-		if (changed())
-			set_value(*m_port, true);
-
-		return m_value;
-	}
+		{ probe(); return m_value; }
 
 	float operator *() { return value(1); }
 
 protected:
 
-	bool changed() const
-		{ return (m_port && ::fabsf(*m_port - m_value) > 0.001f); }
+	void probe()
+	{
+		if (m_port && ::fabsf(*m_port - m_value) > 0.001f) {
+			if (m_cache)
+				update();
+			else
+				set_value(*m_port, true);
+		}
+	}
 
-	void update()
-		{ if (m_port) *m_port = m_value; }
+	void update() { if (m_port) *m_port = m_value; m_cache = false; }
 
 	float *m_port;
 	float  m_value;
+	bool   m_cache;
 };
 
 
@@ -188,9 +190,11 @@ public:
 
 	samplv1_port2() : m_vstep(0.0f), m_nstep(0) {}
 
-	void set_value(float value, bool cache = false)
+	void set_value(float value, bool cache)
 	{
-		if (cache) {
+		m_cache = cache;
+
+		if (m_cache) {
 			m_nstep = NSTEP;
 			m_vstep = (value - m_value) / float(m_nstep);
 		} else {
@@ -202,8 +206,8 @@ public:
 
 	float value(uint32_t nstep = NSTEP)
 	{
-		if (m_nstep == 0 && changed())
-			set_value(*m_port, true);
+		if (m_nstep == 0)
+			probe();
 
 		if (m_nstep > 0) {
 			if (m_nstep >= nstep) {
