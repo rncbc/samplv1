d03b3d2ba0b4ee98812700ff30b46671e0d76b02
rncbc <rncbc@rncbc.org>
2017-10-25 17:34:33 +0100

    - Sample picker now opens to same directory and location, when applicable.

--- src/samplv1widget_sample.cpp
+++ src/samplv1widget_sample.cpp
@@ -575,7 +575,9 @@ void samplv1widget_sample::openSample (void)
 	if (pConfig == NULL)
 		return;
 
-	QString sFilename;
+	QString sFilename = pConfig->sSampleDir;
+	if (m_pSample && m_pSample->filename())
+		sFilename = QString::fromUtf8(m_pSample->filename());
 
 	// Cache supported file-types stuff from libsndfile...
 	static QStringList s_filters;
@@ -621,10 +623,9 @@ void samplv1widget_sample::openSample (void)
 	}
 #if 1//QT_VERSION < 0x040400
 	sFilename = QFileDialog::getOpenFileName(pParentWidget,
-		sTitle, pConfig->sSampleDir, sFilter, NULL, options);
+		sTitle, sFilename, sFilter, NULL, options);
 #else
-	QFileDialog fileDialog(pParentWidget,
-		sTitle, pConfig->sSampleDir, sFilter);
+	QFileDialog fileDialog(pParentWidget, sTitle, sFilename, sFilter);
 	fileDialog.setAcceptMode(QFileDialog::AcceptOpen);
 	fileDialog.setFileMode(QFileDialog::ExistingFile);
 	QList<QUrl> urls(fileDialog.sidebarUrls());
