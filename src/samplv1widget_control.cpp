446aee4b2562b1e988cf8731a2605993b16cd4b1
rncbc <rncbc@rncbc.org>
2017-12-28 23:32:41 +0000

    - Fixed several .ui header title comments.

--- src/samplv1widget_control.cpp
+++ src/samplv1widget_control.cpp
@@ -226,7 +226,7 @@ void samplv1widget_control::closeEvent ( QCloseEvent *pCloseEvent )
 void samplv1widget_control::setControlKey ( const samplv1_controls::Key& key )
 {
 	// Anti-flooding guard timer < 3sec...
-	if (m_pEventTimer->elapsed() < 3000)
+	if (m_iDirtySetup == 0 && m_pEventTimer->elapsed() < 3000)
 		return;
 
 	setControlType(key.type());
