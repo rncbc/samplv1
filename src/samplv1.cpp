f16ec9a655afce1e76f4b6676ad843d9760e92d1
rncbc <rncbc@rncbc.org>
2019-02-21 15:46:25 +0000

    - Kinda regression from original reverse, offset, loop range catch-up changes.

--- src/samplv1.cpp
+++ src/samplv1.cpp
@@ -244,7 +244,7 @@ class samplv1_port3 : public samplv1_port
 public:
 
 	samplv1_port3(samplv1_sched *sched, samplv1::ParamIndex index)
-		: m_sched(sched), m_index(index), m_vsync(0.5f), m_xsync(false) {}
+		: m_sched(sched), m_index(index), m_vsync(0.0f), m_xsync(false) {}
 
 	void set_value(float value)
 	{
@@ -253,7 +253,7 @@ public:
 			const float v1 = m_vsync;
 			const float d1 = ::fabsf(v1 - value);
 			const float d2 = ::fabsf(v1 - v0) * d1;
-			m_xsync = (d2 < 0.5f);
+			m_xsync = (d2 < 0.001f);
 		}
 
 		samplv1_port::set_value(value);
@@ -263,13 +263,7 @@ public:
 	}
 
 	void set_value_sync(float vsync, bool xsync)
-	{
-		m_vsync = vsync;
-		m_xsync = xsync;
-
-		if (!xsync) samplv1_port::set_value(vsync);
-	}
-
+		{ m_vsync = vsync; m_xsync = xsync; }
 	float value_sync() const
 		{ return m_vsync; }
 	bool is_sync() const
