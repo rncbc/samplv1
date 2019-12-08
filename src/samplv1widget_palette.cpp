4fe78a281837fcfd8d5b7f572e984d519adc042b
rncbc <rncbc@rncbc.org>
2019-12-08 15:58:06 +0000

    - Fix one latest FTBFS...

--- src/samplv1widget_palette.cpp
+++ src/samplv1widget_palette.cpp
@@ -828,7 +828,7 @@ void samplv1widget_palette::reject (void)
 void samplv1widget_palette::setDefaultDir ( const QString& dir )
 {
 	if (m_settings) {
-		m_settings->beginGroup(samplv1widget_paletteGroup);
+		m_settings->beginGroup(PaletteEditorGroup);
 		m_settings->setValue(DefaultDirKey, dir);
 		m_settings->endGroup();
 	}
@@ -840,7 +840,7 @@ QString samplv1widget_palette::defaultDir (void) const
 	QString dir;
 
 	if (m_settings) {
-		m_settings->beginGroup(samplv1widget_paletteGroup);
+		m_settings->beginGroup(PaletteEditorGroup);
 		dir = m_settings->value(DefaultDirKey).toString();
 		m_settings->endGroup();
 	}
@@ -852,7 +852,7 @@ QString samplv1widget_palette::defaultDir (void) const
 void samplv1widget_palette::setShowDetails ( bool on )
 {
 	if (m_settings) {
-		m_settings->beginGroup(samplv1widget_paletteGroup);
+		m_settings->beginGroup(PaletteEditorGroup);
 		m_settings->setValue(ShowDetailsKey, on);
 		m_settings->endGroup();
 	}
@@ -864,7 +864,7 @@ bool samplv1widget_palette::isShowDetails (void) const
 	bool on = false;
 
 	if (m_settings) {
-		m_settings->beginGroup(samplv1widget_paletteGroup);
+		m_settings->beginGroup(PaletteEditorGroup);
 		on = m_settings->value(ShowDetailsKey).toBool();
 		m_settings->endGroup();
 	}
