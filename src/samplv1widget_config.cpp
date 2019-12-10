4b27a9def46f51d89776d90614d359b2006263c0
rncbc <rncbc@rncbc.org>
2019-12-10 16:12:01 +0000

    - Fixed for recent deleted color theme setting.

--- src/samplv1widget_config.cpp
+++ src/samplv1widget_config.cpp
@@ -869,9 +869,12 @@ void samplv1widget_config::resetCustomColorThemes (
 		samplv1widget_palette::namedPaletteList(pConfig));
 
 	int iCustomColorTheme = 0;
-	if (!sCustomColorTheme.isEmpty())
+	if (!sCustomColorTheme.isEmpty()) {
 		iCustomColorTheme = m_ui.CustomColorThemeComboBox->findText(
 			sCustomColorTheme);
+		if (iCustomColorTheme < 0)
+			iCustomColorTheme = 0;
+	}
 	m_ui.CustomColorThemeComboBox->setCurrentIndex(iCustomColorTheme);
 }
 
@@ -887,9 +890,12 @@ void samplv1widget_config::resetCustomStyleThemes (
 		QStyleFactory::keys());
 
 	int iCustomStyleTheme = 0;
-	if (!sCustomStyleTheme.isEmpty())
+	if (!sCustomStyleTheme.isEmpty()) {
 		iCustomStyleTheme = m_ui.CustomStyleThemeComboBox->findText(
 			sCustomStyleTheme);
+		if (iCustomStyleTheme < 0)
+			iCustomStyleTheme = 0;
+	}
 	m_ui.CustomStyleThemeComboBox->setCurrentIndex(iCustomStyleTheme);
 }
 
