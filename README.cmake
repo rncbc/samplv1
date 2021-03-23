e8236902506bec300be33208c47e7f577f63f86d
rncbc <rncbc@rncbc.org>
2021-03-23 12:19:58 +0000

    - Reverted to former NSM designation.

--- README.cmake
+++ README.cmake
@@ -72,6 +72,4 @@ configuration process, this means that there may be an error in the source code,
 or in the build system, or something incompatible in 3rd party libraries.
 
 The CMake build system for samplv1 is experimental. It will take a while
-until it becomes stable and fully tested. You can help providing feedback, 
-please send a report containing your problems to the samplv1 development 
-mailing list, https://lists.sourceforge.net/mailman/listinfo/samplv1-devel
+until it becomes stable and fully tested.
