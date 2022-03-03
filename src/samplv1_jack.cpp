3dddbf685918df253634e609b4ed90d90083b024
rncbc <rncbc@rncbc.org>
2022-03-03 16:18:08 +0000

    - Set QApplication::desktopFileName()...

--- src/samplv1_jack.cpp
+++ src/samplv1_jack.cpp
@@ -782,6 +782,10 @@ samplv1_jack_application::samplv1_jack_application ( int& argc, char **argv )
 	#if QT_VERSION >= QT_VERSION_CHECK(5, 1, 0)
 		pApp->setApplicationDisplayName(SAMPLV1_TITLE);
 		//	SAMPLV1_TITLE " - " + QObject::tr(SAMPLV1_SUBTITLE));
+	#if QT_VERSION >= QT_VERSION_CHECK(5, 7, 0)
+		pApp->setDesktopFileName(
+			QString("org.rncbc.%1").arg(PACKAGE_TARNAME));
+	#endif
 	#endif
 		m_pApp = pApp;
 	} else {
