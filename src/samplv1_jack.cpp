0412f9f9c819b870042f545b02e9d82097225bcb
rncbc <rncbc@rncbc.org>
2023-02-02 10:42:29 +0000

    - JACK client watchdog introduced for automatic reactivation and resilience (standalone only). (EXPERIMENTAL)

--- src/samplv1_jack.cpp
+++ src/samplv1_jack.cpp
@@ -1,7 +1,7 @@
 // samplv1_jack.cpp
 //
 /****************************************************************************
-   Copyright (C) 2012-2022, rncbc aka Rui Nuno Capela. All rights reserved.
+   Copyright (C) 2012-2023, rncbc aka Rui Nuno Capela. All rights reserved.
 
    This program is free software; you can redistribute it and/or
    modify it under the terms of the GNU General Public License
@@ -723,6 +723,8 @@ void samplv1_jack::shutdown_close (void)
 #endif
 #endif
 
+#include <QTimer>
+
 #ifdef CONFIG_NSM
 #include "samplv1_nsm.h"
 #endif
@@ -1049,6 +1051,9 @@ bool samplv1_jack_application::setup (void)
 	if (m_pWidget)
 		m_pWidget->show();
 
+	// Start watchdog timer...
+	watchdog_start();
+
 	return true;
 }
 
@@ -1199,6 +1204,29 @@ void samplv1_jack_application::handle_sigterm (void)
 #endif	// HAVE_SIGNAL_H
 
 
+// Simple watchdog (3 minute cycle).
+void samplv1_jack_application::watchdog_slot (void)
+{
+	if (m_pSampl && m_pSampl->client() == nullptr) {
+		const QByteArray aClientName
+			= m_sClientName.toLocal8Bit();
+		const char *client_name
+			= aClientName.constData();
+		m_pSampl->open(client_name);
+		m_pSampl->activate();
+	}
+
+	watchdog_start();
+}
+
+
+void samplv1_jack_application::watchdog_start (void)
+{
+	if (g_pInstance)
+		QTimer::singleShot(180000, this, SLOT(watchdog_slot()));
+}
+
+
 // JACK shutdown handlers.
 void samplv1_jack_application::shutdown (void)
 {
@@ -1208,16 +1236,18 @@ void samplv1_jack_application::shutdown (void)
 
 void samplv1_jack_application::shutdown_slot (void)
 {
-	bool bQuit = true;
-
 	if (m_pSampl)
 		m_pSampl->shutdown_close();
+
+#if 0//Don't quit anymore; let watchdog handle auto re-activation...
+	bool bQuit = true;
 
 	if (m_pWidget)
 		bQuit = m_pWidget->queryClose();
 
 	if (m_pApp && bQuit)
 		m_pApp->quit();
+#endif
 }
 
 
