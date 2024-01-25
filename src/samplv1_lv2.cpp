24dedebbff322dd0b7ec3e94c9fd6fff761fa73d
rncbc <rncbc@rncbc.org>
2024-01-25 10:30:44 +0000

    - Added build checks on whether to use old or newer style of LV2 include headers. (final)

--- src/samplv1_lv2.cpp
+++ src/samplv1_lv2.cpp
@@ -27,7 +27,7 @@
 #include "samplv1_programs.h"
 #include "samplv1_controls.h"
 
-#ifdef CONFIG_OLD_HEADERS
+#ifdef CONFIG_LV2_OLD_HEADERS
 #include "lv2/lv2plug.in/ns/ext/midi/midi.h"
 #include "lv2/lv2plug.in/ns/ext/time/time.h"
 #include "lv2/lv2plug.in/ns/ext/atom/util.h"
